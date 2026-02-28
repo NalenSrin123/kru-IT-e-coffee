977cf2fc88f0639b3beedece2914478f49ee4682
MaoSreyPich <maosreypich771@gmail.com>
2026-02-28 13:37:22 +0700

    update migration

--- database/migrations/2026_02_28_051519_create_payments_table.php
+++ database/migrations/2026_02_28_051519_create_payments_table.php
@@ -13,7 +13,28 @@ return new class extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('order_id');
+            $table->unsignedBigInteger('cashier_id');
+
+            // ENUM Payment Method
+            $table->enum('payment_method', ['cash', 'card', 'khqr'])->default('cash');
+
+            $table->decimal('amount_paid', 10, 2);
+
+            // POS Cash Handling
+            $table->decimal('tendered_amount', 10, 2)->nullable();
+            $table->decimal('change_amount', 10, 2)->default(0.00);
+
+            $table->string('reference_number')->nullable();
+
+            $table->enum('status', ['pending', 'completed', 'failed', 'refunded'])
+                  ->default('completed');
+
             $table->timestamps();
+
+            // Foreign Key Constraints
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+            $table->foreign('cashier_id')->references('id')->on('users')->onDelete('set null');
         });
     }
 
